b5b14f69457d09204f5a7323f18f2ac4400201c3
Patrick Tamayo <Patrick Tamayo>
2017-05-09 20:41:28 -0700

    first commit

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour {
+
+	[SerializeField] private float objectSpeed = 1;
+	private float resetPosition = -26.24f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * (objectSpeed * Time.deltaTime));	
+		if (transform.localPosition.x <= resetPosition) {
+			Vector3 newPos = new Vector3 (72.01f, transform.position.y, transform.position.z);
+			transform.position = newPos;
+		}
+	}
+}
