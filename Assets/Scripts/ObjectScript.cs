66d95f1dbbbd713b332715a4a4e37bdea08803b2
Patrick Tamayo <Patrick Tamayo>
2017-05-11 02:15:29 -0700

    adding moving stones, character model and animation

--- Assets/Scripts/ObjectScript.cs
+++ Assets/Scripts/ObjectScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ObjectScript : MonoBehaviour {
 
 	[SerializeField] private float objectSpeed = 1;
-	private float resetPosition = -26.24f;
+	[SerializeField] private float resetPosition = -26.24f;
+	[SerializeField] private float startPosition = 72.01f;
 
 
 	// Use this for initialization
@@ -14,10 +15,10 @@ public class ObjectScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	protected virtual void Update () {
 		transform.Translate (Vector3.left * (objectSpeed * Time.deltaTime));	
 		if (transform.localPosition.x <= resetPosition) {
-			Vector3 newPos = new Vector3 (72.01f, transform.position.y, transform.position.z);
+			Vector3 newPos = new Vector3 (startPosition, transform.position.y, transform.position.z);
 			transform.position = newPos;
 		}
 	}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : ObjectScript {
+
+	[SerializeField] Vector3 topPosition;
+	[SerializeField] Vector3 bottomPosition;
+	[SerializeField] float rockSpeed;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Move (bottomPosition));
+	}
+
+	protected override void Update(){
+		base.Update ();
+	}
+
+	IEnumerator Move(Vector3 target){
+		while (Mathf.Abs((target - transform.localPosition).y) > 0.20f) {
+			Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+			transform.localPosition += direction * Time.deltaTime* rockSpeed;
+			yield return null;
+		}
+		print ("Reached the target");
+		yield return new WaitForSeconds (0.5f);
+
+		Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+		StartCoroutine (Move (newTarget));
+	}
+}
